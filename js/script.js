8c2330305c3c56673bad172bb295639eb6c2a1b9
Cobbina Emmanuel <kobbyj099@gmail.com>
2023-01-14 21:52:04 -0800

    Modified

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
-const update_bt = document.querySelector(".update-btn");
-update_bt.addEventListener('click', function () {
-    console.log("Hello world")
-})
+const update_btn = document.querySelector(".update-btn");
+const countryName = document.querySelector(".enter-country-name");
+const activeCase = document.querySelector(".active-case");
+const confirmCase = document.querySelector(".confirm-case");
+const death = document.querySelector(".death");
+const recovery = document.querySelector(".recovery");
+const lastUpdate = document.querySelector(".last-update");
 
+update_btn.addEventListener('click', getApiData());
 
 
 async function getApiData() {
-    const apiRequest = await fetch("http://localhost/timeseries/confirmed");
-    const jsonData = await apiRequest.json()
-    console.log(jsonData)
+  const apiUrl = `https://api.covid19api.com/total/country/${countryName.value}`;
+  const apiRequest = await fetch(apiUrl);
+  const jsonData = await apiRequest.json();
+  const updateItem = jsonData.length;
+  const lastUpdateItem = updateItem - 1;
+  const updateToReturn = jsonData[lastUpdateItem];
+  console.log(updateToReturn.Active);
 }
-getApiData()
