a939802fb5a220dc99d7c872445d577b8c27cc6a
Cobbina Emmanuel <kobbyj099@gmail.com>
2023-01-19 11:58:15 -0800

    Added function to handle all countries

--- scipt.js
+++ scipt.js
@@ -2,56 +2,53 @@ const table_body = document.querySelector(".table-body");
 const search_button = document.querySelector(".search-button");
 const form_control = document.querySelector(".form-control");
 
-async function getCovidApi() {
+// Function to retrieve Total data information
+async function getAll() {
     const request = await fetch("https://disease.sh/v3/covid-19/all");
-    const json = await request.json();
-    console.log(json)
+    const data = await request.json();
     table_body.innerHTML = `
-    <tr>
-                
-                <td>All</td>
-                <td>${json.population}</td>
-                <td>${json.cases}</td>
-                <td>${json.todayCases}</td>
-                <td>${json.deaths}</td>
-                <td>${json.todayDeaths}</td>
-                <td>${json.recovered}</td>
-                <td>${json.todayRecovered}</td>
-                <td>${json.active}</td>
-                <td>${json.critical}</td>
-                <td>${json.tests}</td>
-                
-                
-            </tr>
+    <tr>       
+        <td class="fw-bolder text-success table-warning">Total</td>
+        <td class="table-warning fw-bold">${data.population}</td>
+        <td class="table-warning fw-bold">${data.cases}</td>
+        <td class="table-warning fw-bold">${data.todayCases}</td>
+        <td class="table-warning fw-bold">${data.deaths}</td>
+        <td class="table-warning fw-bold">${data.todayDeaths}</td>
+        <td class="table-warning fw-bold">${data.recovered}</td>
+        <td class="table-warning fw-bold">${data.todayRecovered}</td>
+        <td class="table-warning fw-bold">${data.active}</td>
+        <td class="table-warning fw-bold">${data.critical}</td>
+        <td class="table-warning fw-bold">${data.tests}</td> 
+    </tr>
     `;
-
 }
-getCovidApi();
-search_button.addEventListener("click", function () {
-  console.log(form_control.value);
-  getCountry();
-});
-async function getCountry() {
-    const countries = form_control.value
-  const request = await fetch("https://disease.sh/v3/covid-19/countries/" + countries);
+// Call the getAll() function
+getAll();
+
+// Function to retrieve data country by country
+async function getCountries() {
+  const request = await fetch("https://disease.sh/v3/covid-19/countries");
   const data = await request.json();
-    console.log(data);
-  table_body.innerHTML += `
-    <tr>
-                
-                <td>${data.country}</td>
-                <td>${data.population}</td>
-                <td>${data.cases}</td>
-                <td>${data.todayCases}</td>
-                <td>${data.deaths}</td>
-                <td>${data.todayDeaths}</td>
-                <td>${data.recovered}</td>
-                <td>${data.todayRecovered}</td>
-                <td>${data.active}</td>
-                <td>${data.critical}</td>
-                <td>${data.tests}</td>
-            </tr>
+  for (let i = 0; i < data.length; i++) {
+    table_body.innerHTML += `
+    <tr>     
+        <td>${data[i].country}</td>
+        <td>${data[i].population}</td>
+        <td>${data[i].cases}</td>
+        <td>${data[i].todayCases}</td>
+        <td>${data[i].deaths}</td>
+        <td>${data[i].todayDeaths}</td>
+        <td>${data[i].recovered}</td>
+        <td>${data[i].todayRecovered}</td>
+        <td>${data[i].active}</td>
+        <td>${data[i].critical}</td>
+        <td>${data[i].tests}</td>
+    </tr>
     `;
+  }
 }
+// Call the getCountries() function
+getCountries()
 
 
+// Function for retrieving just one country
