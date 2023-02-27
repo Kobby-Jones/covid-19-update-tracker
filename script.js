2b2d85b221746ecc978739554750e2edff0790c5
Cobbina Emmanuel <kobbyj099@gmail.com>
2023-02-27 07:41:49 +0000

    Country data

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const table_body = document.querySelector("#table-body");
 const search = document.getElementById('search');
 
-
+// Create a function for the world total row
 function tableBody(data) {
-  // Create the tr element
   let tr = document.createElement("tr");
   let td1 = document.createElement("td");
   td1.classList.add("fw-bolder", "text-success", "table-warning")
@@ -50,8 +49,6 @@ function tableBody(data) {
   td11.innerText = data.tests;
   tr.appendChild(td11);
   table_body.appendChild(tr);
-
-
 }
   // Function to retrieve Total data information
   async function getAll() {
@@ -62,29 +59,53 @@ function tableBody(data) {
   // Call the getAll() function
 getAll();
   
+// Create another function for the countries rows
+function countriesData(countryData) {
+  let tr = document.createElement("tr");
+  tr.classList.add("country")
+   let td1 = document.createElement("td");
+   td1.innerText = countryData.country
+   tr.appendChild(td1);
+   let td2 = document.createElement("td");
+   td2.innerText = countryData.population;
+   tr.appendChild(td2);
+   let td3 = document.createElement("td");
+   td3.innerText = countryData.cases;
+   tr.appendChild(td3);
+   let td4 = document.createElement("td");
+   td4.innerText = countryData.todayCases;
+   tr.appendChild(td4);
+   let td5 = document.createElement("td");
+   td5.innerText = countryData.deaths;
+   tr.appendChild(td5);
+   let td6 = document.createElement("td");
+   td6.innerText = countryData.todayDeaths;
+   tr.appendChild(td6);
+   let td7 = document.createElement("td");
+   td7.innerText = countryData.recovered;
+   tr.appendChild(td7);
+   let td8 = document.createElement("td");
+   td8.innerText = countryData.todayRecovered;
+   tr.appendChild(td8);
+   let td9 = document.createElement("td");
+   td9.innerText = countryData.active;
+   tr.appendChild(td9);
+   let td10 = document.createElement("td");
+   td10.innerText = countryData.critical;
+   tr.appendChild(td10);
+   let td11 = document.createElement("td");
+   td11.innerText = countryData.tests;
+   tr.appendChild(td11);
+   table_body.appendChild(tr);
+}
 
   // Function to retrieve data country by country
   async function getCountries() {
     const request = await fetch("https://disease.sh/v3/covid-19/countries");
-    const data = await request.json();
-    for (let i = 0; i < data.length; i++) {
-      table_body.innerHTML += `
-    <tr class = "country">     
-        <td >${data[i].country}</td>
-        <td>${data[i].population}</td>
-        <td>${data[i].cases}</td>
-        <td>${data[i].todayCases}</td>
-        <td>${data[i].deaths}</td>
-        <td>${data[i].todayDeaths}</td>
-        <td>${data[i].recovered}</td>
-        <td>${data[i].todayRecovered}</td>
-        <td>${data[i].active}</td>
-        <td>${data[i].critical}</td>
-        <td>${data[i].tests}</td>
-    </tr>
-    `;
-      
-    }
+    const countryData = await request.json();
+    countryData.forEach(data => {
+      countriesData(data);
+   })
   }
   // Call the getCountries() function
 getCountries();
